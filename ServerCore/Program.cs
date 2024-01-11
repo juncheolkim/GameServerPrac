f08fe1a011dc55d36012ba0d948dcaa1549a2cdc
juncheolkim <kimjoon889@gmail.com>
2024-01-11 16:17:13 +0900

    Mutex로 Lock 구현

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,42 +1,26 @@
 ﻿namespace ServerCore
 {
-    class Lock
-    {
-        // bool <- 커널이 관리하는 변수
-        AutoResetEvent _available = new AutoResetEvent(true);
-
-        public void Acquire()
-        {
-            _available.WaitOne();   // 입장 시도 -> 입장하면 자동으로 문 닫는다
-            //_available.Reset(); // bool = false , 이 작업을 WaitOne이 알아서 한다.
-        }
-
-        public void Release()
-        {
-            _available.Set();   // flag = true
-        }
-    }
     internal class Program
     {
         static int _num = 0;
-        static Lock _lock = new Lock();
+        static Mutex _lock = new Mutex();
         static void Thread_1()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num++;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                _lock.Acquire();
+                _lock.WaitOne();
                 _num--;
-                _lock.Release();
+                _lock.ReleaseMutex();
             }
         }
 
