23b063feb05fb2ba568de077b540dcd64fc2cea2
juncheolkim <kimjoon889@gmail.com>
2024-01-24 22:54:55 +0900

    ClientSession과 ServerSession 생성 packet 직렬화와 역직렬화 구현, 자동화 가능한 코드를 염두하고 작성

---
+++ DummyClient/ServerSession.cs
@@ -0,0 +1,94 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+    class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+    }
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+    }
+
+    class PlayerInfoOk : Packet
+    {
+        public int hp;
+        public int attack;
+    }
+
+    public enum PacketID
+    {
+        PlaterInfoReq = 1,
+        PlaterInfoOk = 2,
+    }
+
+
+    class ServerSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected: {endPoint}");
+
+            PlayerInfoReq packet = new PlayerInfoReq() { packetId = (ushort)PacketID.PlaterInfoReq, playerId = 1001};
+
+            // 보낸다
+            //for (int i = 0; i < 5; i++)
+            {
+                ArraySegment<byte> s = SendBufferHelper.Open(4096);
+
+                bool success = true;
+                ushort count = 0;
+
+                //success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), packet.size);
+                count += 2;
+                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.packetId);
+                count += 2;
+                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset + count, s.Count - count), packet.playerId);
+                count += 8;
+                success &= BitConverter.TryWriteBytes(new Span<byte>(s.Array, s.Offset, s.Count), count);
+
+
+                //byte[] size = BitConverter.GetBytes(packet.size);               // 2byte
+                //byte[] packetId = BitConverter.GetBytes(packet.packetId);       // 2byte
+                //byte[] playerId = BitConverter.GetBytes(packet.playerId);       // 8byte
+
+                //Array.Copy(size, 0, s.Array, s.Offset + count, 2);
+                //count += 2;
+                //Array.Copy(packetId, 0, s.Array, s.Offset + count, 2);
+                //count += 2;
+                //Array.Copy(playerId, 0, s.Array, s.Offset + count, 8);
+                //count += 8;
+
+                ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
+
+                if (success)
+                    Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected: {endPoint}");
+        }
+
+        public override int OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Server] {recvData}");
+            return buffer.Count;
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transfered bytes: {numOfBytes}");
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -5,54 +5,6 @@ using ServerCore;
 
 namespace Server
 {
-    class Packet
-    {
-        public ushort size;
-        public ushort packetId;
-    }
-
-    class LoginOkPacket : Packet
-    {
-
-    }
-
-    class GameSession : PacketSession
-    {
-        public override void OnConnected(EndPoint endPoint)
-        {
-            Console.WriteLine($"OnConnected: {endPoint}");
-
-            //Packet packet = new Packet() { size = 100, packetId = 10 };
-
-            //ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-            //byte[] buffer = BitConverter.GetBytes(packet.size);
-            //byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
-            //Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
-            //Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
-            //ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
-
-            //Send(sendBuff);
-            Thread.Sleep(5000);
-            Disconnect();
-        }
-        public override void OnRecvPacket(ArraySegment<byte> buffer)
-        {
-            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
-            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + 2);
-            Console.WriteLine($"RecvPacketId: {id}, Size: {size}");
-        }
-
-        public override void OnDisconnected(EndPoint endPoint)
-        {
-            Console.WriteLine($"OnDisconnected: {endPoint}");
-        }
-
-
-        public override void OnSend(int numOfBytes)
-        {
-            Console.WriteLine($"Transfered bytes: {numOfBytes}");
-        }
-    }
     internal class Program
     {
         static Listener _listener = new Listener();
@@ -65,7 +17,7 @@ namespace Server
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            _listener.Init(endPoint, () => { return new GameSession(); });
+            _listener.Init(endPoint, () => { return new ClientSession(); });
             Console.WriteLine("Listening ...");
             while (true)
             {
