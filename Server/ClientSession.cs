12c92d23c0b463af33cf226f26d4bcf88c2faedb
juncheolkim <kimjoon889@gmail.com>
2024-01-25 22:24:54 +0900

    패킷 리스트 직렬화 구현 - 프로토 버퍼

--- Server/ClientSession.cs
+++ Server/ClientSession.cs
@@ -21,6 +21,38 @@ namespace Server
         public long playerId;
         public string name;
 
+        public struct Skillinfo
+        {
+            public int id;
+            public short level;
+            public float duration;
+
+            public bool Write(Span<byte> s, ref ushort count)
+            {
+                bool success = true;
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), id);
+                count += sizeof(int);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), level);
+                count += sizeof(short);
+                success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), duration);
+                count += sizeof(float);
+
+                return success;
+            }
+
+            public void Read(ReadOnlySpan<byte> s, ref ushort count)
+            {
+                id = BitConverter.ToInt32(s.Slice(count, s.Length - count));
+                count += sizeof(int);
+                level = BitConverter.ToInt16(s.Slice(count, s.Length - count));
+                count += sizeof(short);
+                duration = BitConverter.ToSingle(s.Slice(count, s.Length - count));
+                count += sizeof(float);
+            }
+        }
+
+        public List<Skillinfo> skills = new List<Skillinfo>();
+
         public PlayerInfoReq()
         {
             this.packetId = (ushort)PacketID.PlaterInfoReq;
@@ -31,9 +63,7 @@ namespace Server
             ushort count = 0;
 
             ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
-            //ushort size = BitConverter.ToUInt16(s.Array, s.Offset);
             count += sizeof(ushort);
-            // ushort id = BitConverter.ToUInt16(s.Array, s.Offset + count);
             count += sizeof(ushort);
             // ReadOnlySpan은 읽기전용이기 때문에, 범위가 이상할 경우 예외를 발생시킨다.
             this.playerId = BitConverter.ToInt64(s.Slice(count, s.Length - count));
@@ -43,6 +73,18 @@ namespace Server
             ushort nameLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
             count += sizeof(ushort);
             this.name = Encoding.Unicode.GetString(s.Slice(count, nameLen));
+            count += nameLen;
+
+            // skill list
+            ushort skillLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+            count += sizeof(ushort);
+            skills.Clear();
+            for (int i = 0; i < skillLen; i++)
+            {
+                Skillinfo skill = new Skillinfo();
+                skill.Read(s, ref count);
+                skills.Add(skill);
+            }
         }
 
         public override ArraySegment<byte> Write()
@@ -61,11 +103,17 @@ namespace Server
             count += sizeof(long);
 
             // string
-            ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(this.name);
+            ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.name, 0, this.name.Length, segment.Array, segment.Offset + count + sizeof(ushort));
             success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), nameLen);
             count += sizeof(ushort);
-            Array.Copy(Encoding.Unicode.GetBytes(this.name), 0, segment.Array, count, nameLen); count += sizeof(ushort);
             count += nameLen;
+
+            // skill list
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)skills.Count);
+            count += sizeof(ushort);
+            foreach (Skillinfo skill in skills)
+                success &= skill.Write(s, ref count);
+
             success &= BitConverter.TryWriteBytes(s, count);
 
             if (success == false)
@@ -114,6 +162,11 @@ namespace Server
                         PlayerInfoReq p = new PlayerInfoReq();
                         p.Read(buffer);
                         Console.WriteLine($"PlayerInfoReq: {p.playerId} {p.name}");
+
+                        foreach(PlayerInfoReq.Skillinfo skill in p.skills)
+                        {
+                            Console.WriteLine($"Skill({skill.id})({skill.level})({skill.duration})");
+                        }
                     }
                     break;
                 case PacketID.PlaterInfoOk:
